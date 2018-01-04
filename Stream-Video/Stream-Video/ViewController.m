5f14e5d7a6553ffe73d6bbb8926a7b586d3a8b5b
Tanjim Hossain <tanjim@revesoft.com>
2018-01-04 19:39:01 +0600

    Video streaming realted method updated -AVPlayer and AVPlayerViewController added to stream video from the url

--- Stream-Video/Stream-Video/ViewController.m
+++ Stream-Video/Stream-Video/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVKit/AVKit.h>
 
 @interface ViewController ()
 
@@ -25,7 +26,38 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+#pragma mark - Button Action
 - (IBAction)onButtonStream:(id)sender {
+    
+    NSString *urlString = self.urlTextField.text;
+    
+    if(!urlString || urlString.length<=0) {
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:@"Please insert a video URL at the URL field" preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:okAction];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self presentViewController:alert animated:YES completion:nil];
+        });
+    }
+    else {
+        [self playVideoFromURL:urlString];
+    }
+}
+
+#pragma mark - Private methods
+
+-(void) playVideoFromURL: (NSString *) urlString {
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    AVPlayer *avPlayer = [AVPlayer playerWithURL:url];
+    AVPlayerViewController *avPlayerVC = [AVPlayerViewController new];
+    avPlayerVC.player = avPlayer;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self presentViewController:avPlayerVC animated:YES completion:nil];
+    });
 }
 @end
